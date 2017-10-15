8ba6606af7db096e3dfc19e221225248a02ed74d
Elge <info@sebastian-goral.de>
2017-10-15 20:55:21 +0200

    Fix error in version number

--- SFdebris binge mode.user.js
+++ SFdebris binge mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SFdebris binge mode
 // @namespace    http://tampermonkey.net/
-// @version      1.41
+// @version      1.4.1
 // @description  Makes a few changes to sfdebris to make binge watching easier: The site is expanded so videos fill most of the screen by default, the playlist collapse when clicking their header and adds "Next part" and "Next video" buttons to the videos for fast access.
 // @author       Elge
 // @match        http://sfdebris.com/videos/*

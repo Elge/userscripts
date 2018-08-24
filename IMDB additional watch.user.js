e7a92cca4075b5469f2b2d71e4a5f20a3aef7579
Elge <info@sebastian-goral.de>
2018-08-24 20:09:42 +0200

    Update IMDB additional watch.user.js

--- IMDB additional watch.user.js
+++ IMDB additional watch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IMDB additional watch
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.imdb.com/title/*

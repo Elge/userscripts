036914f494e6ec9649d4793677b6df803eeea272
Elge <7862596+Elge@users.noreply.github.com>
2019-05-03 22:26:05 +0200

    auto close window on success

--- Steam Auto Redeem Keys.user.js
+++ Steam Auto Redeem Keys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam auto redeem keys
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
 // @description  Automatically confirms Steam key redemptions.
 // @author       Elge
 // @match        https://store.steampowered.com/account/registerkey?key=*
@@ -11,6 +11,17 @@
 (function() {
     'use strict';
 
+    // Abort if error is displayed
+    if (document.querySelector('#error_display').visible() === true) return;
+
+    // Patch success function to close window
+    window.oldOnRegisterProductKeySuccess = window.OnRegisterProductKeySuccess;
+    window.OnRegisterProductKeySuccess = function() {
+        window.oldOnRegisterProductKeySuccess.apply(this, arguments);
+        window.setTimeout(() => window.close(), 5000);
+    }
+
+    // Tick checkmark and submit key
     document.getElementById('accept_ssa').checked = true;
     window.RegisterProductKey();
 })();

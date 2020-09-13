d287f9d8a79c8e3ee9dc0fc8a17554d70d3e17a4
Elge <7862596+Elge@users.noreply.github.com>
2020-09-13 10:53:02 +0200

    Update SFdebris binge mode.user.js
    Disable dailymotion up-next autoplay

--- SFdebris binge mode.user.js
+++ SFdebris binge mode.user.js
@@ -1,62 +1,17 @@
 // ==UserScript==
 // @name         SFdebris binge mode
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Makes a few changes to sfdebris to make binge watching easier: The site is expanded so videos fill most of the screen by default, the playlist collapse when clicking their header and adds "Next part" and "Next video" buttons to the videos for fast access.
 // @author       Elge
 // @match        *://sfdebris.com/videos/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @run-at       document-ready
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    // Unfloat navigation menu
-    window.addEventListener('load', function() {
-        let nav = $('#nav');
-        nav.css('position', 'unset');
-        nav.css('top', 'unset');
-        nav.css('left', 'unset');
-        nav.css('margin', '0 auto');
-        $('#header').append(nav);
-
-        // Expand viewport and video player
-        $('#wrap').css('width', '90%');
-        $('#header').css('background-position', 'center');
-        $('#content').css('width', '100%');
-        $('#content').css('display', 'table');
-        $('#content').css('background-repeat', 'repeat');
-        $('#main').css('display', 'table-cell');
-        $('#main').css('width', 'initial');
-        $('#main').css('float', 'initial');
-        $('#sidebar').css('display', 'table-cell');
-        $('#sidebar').css('float', 'initial');
-        $('.vidtitle').css('background-repeat', 'repeat');
-        $('.imgcen iframe').wrap('<div style="width: 100%; padding-bottom: 56.25%; position: relative"><div style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; max-height: 100vh" /></div>');
-        $('.imgcen iframe').css('width', '100%');
-        $('.imgcen iframe').css('height', '100%');
-
-        // Mark current video
-        $('li > a[href="' + document.location.href + '"]').css('background', '#555');
-
-        // Next part/video button
-        let nextVideo = $('li > a[href="' + document.location.href + '"]').parent().next().children();
-        let videoheaders = $('div.vidtitle > h1');
-        videoheaders.append(function(index) {
-            if (index == videoheaders.length - 1) {
-                return $('<a href="' + (nextVideo ? nextVideo.attr('href') : '') + '" style="float: right">' + (nextVideo ? 'Next video: ' + nextVideo.text() : '') + '</a>');
-            } else {
-                return $('<a onclick="window.sfDscrollTo(document.getElementById(\'videoiframe' + (index + 1) + '\'));" style="float: right; cursor: pointer">Next part</a>');
-            }
-        });
-        $('iframe').each(function(index) {
-            $(this).attr('id', 'videoiframe' + index);
-        });
-
-        // Scroll to first video
-        window.sfDscrollTo(document.getElementById('videoiframe0'));
-    });
 
     window.sfDscrollTo = function(element) {
         let rect = element.getBoundingClientRect();
@@ -64,4 +19,52 @@
         console.log('Top: ' + rect.top + '; Height: ' + rect.height + '; Window: ' + window.innerHeight);
         window.scrollBy(0, pos);
     };
+
+    let nav = $('#nav');
+    nav.css('position', 'unset');
+    nav.css('top', 'unset');
+    nav.css('left', 'unset');
+    nav.css('margin', '0 auto');
+    $('#header').append(nav);
+
+    // Expand viewport and video player
+    $('#wrap').css('width', '90%');
+    $('#header').css('background-position', 'center');
+    $('#content').css('width', '100%');
+    $('#content').css('display', 'table');
+    $('#content').css('background-repeat', 'repeat');
+    $('#main').css('display', 'table-cell');
+    $('#main').css('width', 'initial');
+    $('#main').css('float', 'initial');
+    $('#sidebar').css('display', 'table-cell');
+    $('#sidebar').css('float', 'initial');
+    $('.vidtitle').css('background-repeat', 'repeat');
+    $('.imgcen iframe').wrap('<div style="width: 100%; padding-bottom: 56.25%; position: relative"><div style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; max-height: 100vh" /></div>');
+    $('.imgcen iframe').css('width', '100%');
+    $('.imgcen iframe').css('height', '100%');
+
+    // Disable dailymotion up-next autoplay
+    if ($('.imgcen iframe').attr('src').indexOf('dailymotion.com') !== -1) {
+        $('.imgcen iframe').attr('src', $('.imgcen iframe').attr('src') + '?queue-enable=false');
+    }
+
+    // Mark current video
+    $('li > a[href="' + document.location.href + '"]').css('background', '#555');
+
+    // Next part/video button
+    let nextVideo = $('li > a[href="' + document.location.href + '"]').parent().next().children();
+    let videoheaders = $('div.vidtitle > h1');
+    videoheaders.append(function(index) {
+        if (index == videoheaders.length - 1) {
+            return $('<a href="' + (nextVideo ? nextVideo.attr('href') : '') + '" style="float: right">' + (nextVideo ? 'Next video: ' + nextVideo.text() : '') + '</a>');
+        } else {
+            return $('<a onclick="window.sfDscrollTo(document.getElementById(\'videoiframe' + (index + 1) + '\'));" style="float: right; cursor: pointer">Next part</a>');
+        }
+    });
+    $('iframe').each(function(index) {
+        $(this).attr('id', 'videoiframe' + index);
+    });
+
+    // Scroll to first video
+    window.sfDscrollTo(document.getElementById('videoiframe0'));
 })();
